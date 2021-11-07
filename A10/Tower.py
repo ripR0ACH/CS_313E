572f755e012d23e8b48334419547da783e47c69f
zach morrison <morrisonizach@gmail.com>
2021-11-07 12:09:22 -0600

    still trying to get this to work

--- A10/Tower.py
+++ A10/Tower.py
@@ -10,19 +10,23 @@ import math
 #     if n > 1 and k > 0:
 #         return num_moves(n - 1) + num_moves(n - k - 1) + 1
 #     return 0
-def stack(n, moves):
-    if n == 1:
-        return moves + n
-    print(moves)
-    if n == 2:
-        return stack(n - 1, moves + n)
-    return stack(n - 1, moves + moves + 1)
+def stack(n, moves = 0, count = 1):
+    if count == n:
+        return 2 * moves + 1
+    if count == 1 or count == 2:
+        return stack(n, moves + count, count + 1)
+    return stack(n, 2 * moves + 1, count + 1)
 
 def tower(n):
-    # if n >= 1:
-    # k = math.floor(n - math.sqrt(2 * n + 1) + 1)
-    print(stack(n, 0))
-    # return 1
+    moves = 0
+    if n <= 3:
+        return stack(n)
+    k = math.floor(n - math.sqrt(2 * n + 1) + 1)
+    moves = stack(k)
+    moves += 2 * stack(n - k - 1) + 2
+    return moves
+
+    
 
 def main():
   # read number of disks and print number of moves
