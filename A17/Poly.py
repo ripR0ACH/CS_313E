3bc1c18adc62136780ec2a898608bbea4c70c313
zach morrison <morrisonizach@gmail.com>
2021-11-10 07:57:26 -0600

    on my laptop

--- A17/Poly.py
+++ A17/Poly.py
@@ -22,9 +22,12 @@ class LinkedList (object):
         self.first = None
     # keep Links in descending order of exponents
     def insert_in_order (self, coeff, exp):
-        pass
-        
-        
+        if self.first == None:
+            self.first = Link(coeff, exp)
+        else:
+            if self.first.next == None and self.first.exp > exp:
+                self.first.next = Link(coeff, exp)
+
     # add polynomial p to this poly nomial and return the sum
     def add (self, p):
         pass
